8ea8e3512f878181a36872c6066622d569caeb32
kyawlintunklt <kyawlin.tun@ascendcorp.com>
2020-10-20 09:33:53 +0630

    kkk

--- src/main/java/com/journaldev/spring/controller/HomeController.java
+++ src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		System.out.println("Home Page  dfdsfsdfddfdsfdsffdsfdsfRequested edited, locale = " + locale);
+		System.out.println("Home Page  Kyaw Lintun locale = " + locale);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
